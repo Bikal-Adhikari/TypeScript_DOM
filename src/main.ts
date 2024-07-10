3b376056bd7acc6b38dbfa660094ea292add49fe
Bikal <bikaladhikari5@gmail.com>
2024-07-10 16:58:19 +1000

    Css added and one add function added

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,24 @@
-console.log(1)
+class Contact {
+  public id: number;
+  public name: string;
+  public phone: number;
+  public isAbroad: boolean;
+  constructor(id: number, name: string, phone: number, isAbroad: boolean) {
+    this.id = id;
+    this.name = name;
+    this.phone = phone;
+    this.isAbroad = isAbroad;
+  }
+}
+
+function addContact(): void {
+  const nameInput: HTMLInputElement = document.getElementById(
+    "name-input"
+  ) as HTMLInputElement;
+  const phoneInput: HTMLInputElement = document.getElementById(
+    "phone-input"
+  ) as HTMLInputElement;
+  const name: string = nameInput.value;
+  const phone: number = Number(phoneInput.value);
+  console.log(name, phone);
+}
